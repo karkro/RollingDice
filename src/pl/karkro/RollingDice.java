4d990612ac1410cb8cae6c091f1e561b3468178f
karkro <karol.krolikowski@gmail.com>
2020-10-24 16:07:03 +0200

    Initial commit

---
+++ src/pl/karkro/RollingDice.java
@@ -0,0 +1,13 @@
+package pl.karkro;
+
+import java.util.Random;
+
+public class RollingDice {
+
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int x = rand.nextInt(6) + 1;
+        System.out.println("You rolled a " + x);
+    }
+}
